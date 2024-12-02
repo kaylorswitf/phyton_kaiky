c86595936ec7d796c56590b7a966f68c5c7e6d30
ikyblossom <kaikydourado30@gmail.com>
2024-12-02 10:00:15 -0300

    aula2variavel

---
+++ Aula2/aula02.py
@@ -0,0 +1,11 @@
+print("Hello, world")
+print("exo3rdgenleaders")
+print("WhatsWrongWithYouMissMinaj")
+print("Annyeonghaseyo")
+
+nome_usuario = "ikyblossom" #tipo -> sr = string
+idade_usuario = 20 # tipo -> int = integer
+altura_usuario = 1.77 # tipo -> tipo = float
+maior_de_idade = True # tipo -> bool = boolean
+
+print("Seja bem vindo" , nome_usuario, "sua idade é", idade_usuario, "sua altura é", altura_usuario, "você é maior de idade?", maior_de_idade)
